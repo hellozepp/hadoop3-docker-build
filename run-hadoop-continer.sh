1a6977fb26031700399f742f261d2b4cf07f45e5
hellozepp <jingtianyy@qq.com>
2022-03-31 00:30:52 +0800

    add

--- run-hadoop-continer.sh
+++ run-hadoop-continer.sh
@@ -1,8 +1,8 @@
 docker build -t hadoop3 .
 
-docker run --hostname=hadoop3 \
-  -p 9870:9870 \
-  -p 8088:8088 \
-  -p 19888:19888 \
-  -p 10002:10002 \
-  --name hadoop3 -d hadoop3
+#docker run --hostname=hadoop3 \
+#  -p 9870:9870 \
+#  -p 8088:8088 \
+#  -p 19888:19888 \
+#  -p 10002:10002 \
+#  --name hadoop3 -d hadoop3
